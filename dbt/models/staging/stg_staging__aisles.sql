820f271e3e5238ef046b8b35527e3b8107cd4a5c
abdiaziz mohamed <gedi5685@gmail.com>
2025-03-24 02:59:25 +0000

    made changes to column names

--- dbt/models/staging/stg_staging__aisles.sql
+++ dbt/models/staging/stg_staging__aisles.sql
@@ -14,13 +14,13 @@ cleaned_aisles as (
     -- Perform any necessary data transformations or cleaning here
     select
         -- identifiers
-        aisle_id,
-        aisle
+        int64_field_0 as aisle_id,
+        string_field_1 as aisle
     from source
 )
 
 -- Return the cleaned data
-select * from cleaned_aisles;
+select * from cleaned_aisles
 -- dbt build --select <model_name> --vars '{'is_test_run': 'false'}'
 {% if var('is_test_run', default=true) %}
 
